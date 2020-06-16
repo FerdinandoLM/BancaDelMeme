fd80e618acc0323c51fbaea2e8762509b5ccdf0f
Timendum <timedum@gmail.com>
2020-06-16 10:09:49 +0200

    Close season 3

--- src/seasonleaderboard.py
+++ src/seasonleaderboard.py
@@ -21,16 +21,6 @@ def main():
     engine = create_engine(config.DB, pool_recycle=60, pool_pre_ping=True)
     session_maker = sessionmaker(bind=engine)
 
-    reddit = praw.Reddit(client_id=config.CLIENT_ID,
-                         client_secret=config.CLIENT_SECRET,
-                         username=config.USERNAME,
-                         password=config.PASSWORD,
-                         user_agent=config.USER_AGENT)
-
-    # We will test our reddit connection here
-    if not utils.test_reddit_connection(reddit):
-        exit()
-
     sess = session_maker()
 
     top_users = sess.query(
@@ -46,15 +36,8 @@ def main():
     for i, user in enumerate(top_users):
         top_users_text += f"{i + 1}|/u/{user.name}|{formatNumber(user.networth)} M€\n"
 
-    with open('stagione02.txt', 'wt') as oo:
+    with open('stagione03.txt', 'wt') as oo:
         oo.write(top_users_text)
-
-    sess.commit()
-
-    # Report the Reddit API call stats
-    rem = int(reddit.auth.limits['remaining'])
-    res = int(reddit.auth.limits['reset_timestamp'] - time.time())
-    logging.info(" -- API calls remaining: %s, resetting in %.2fs", rem, res)
 
     sess.close()
 
